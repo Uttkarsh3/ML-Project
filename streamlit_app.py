cee876e34d84323bd15f737e61ffe93199ea63c5
Uttkarsh Bisht <104181880+Uttkarsh3@users.noreply.github.com>
2025-02-17 17:23:14 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,11 @@
 import streamlit as st
+import pandas as pd
 
 st.title('📌 Smart Home Energy Management System')
 
 st.info('An end-to-end pipeline using Energy data for prediction electricity demand!')
+
+df = pd.read_csv('https://www.kaggle.com/datasets/uciml/electric-power-consumption-data-set/data', sep=';',
+                  parse_dates={'dt' : ['Date', 'Time']}, dayfirst=True, 
+                 low_memory=False, na_values=['nan','?'],index_col='dt')
+df
